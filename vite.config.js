b376425ac89249b7c2f7a0e6b81f3a3e74afc99b
pydivijay <vijaykumar519.pydi@gmail.com>
2025-01-31 12:00:47 +0530

    publishing to git pages

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import react from "@vitejs/plugin-react";
 import path from "path";
 
 export default defineConfig({
+  base: "https://github.com/pydivijay/experience-calculator",
   plugins: [react()],
   resolve: {
     alias: {
